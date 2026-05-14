70fc015e976b8c5b947908db44ebf9a12b7d4885
Masstell <matt@mswensen.com>
2026-05-14 20:38:30 +0000

    ohmyposh should not reinstall if already there, and the bashrc needs to happen after install because ohmyposh will brick the bashrc

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ ln -svf ~/.dotfiles/bash_profile ~/.bash_profile
 ln -svf ~/.dotfiles/bashrc ~/.bashrc
 cp ~/.dotfiles/gitconfig_base ~/.gitconfig
 ln -svf ~/.dotfiles/vimrc ~/.vimrc
-curl -s https://ohmyposh.dev/install.sh | bash -s
+command -v oh-my-posh || curl -s https://ohmyposh.dev/install.sh | bash -s
+ln -svf ~/.dotfiles/bashrc ~/.bashrc
 
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undos
 mkdir -p ~/.local/bin
